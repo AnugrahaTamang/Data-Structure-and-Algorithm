bb94a3ff1d9bed4844313cb2154c5cac28e1a79a
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-10 15:30:56 +0545

    sorting

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+const myarr = [4, 2, 6, 1, 3, 5];
+const res = insertionSort(myarr);
+console.log(res);
